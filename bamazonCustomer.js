8bfc28e8aafdf51d568a1ec817ae5b559c704330
herbertg14 <herbert.gutierrez@utexas.edu>
2016-07-16 18:57:03 -0500

    cleaned connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,17 +4,17 @@ var inquirer = require('inquirer');
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "root", //Your username
-    password: "2SANSALVA", //Your password
+    user: "root", 
+    password: "2SANSALVA", 
     database: "bamazon"
 })
 
 connection.connect(function(err) {
     if (err) throw err;
 	console.log("connected as id " + connection.threadId);
+	console.log();
 	display();
 	start();
-	connection.end();
 
 })
 
@@ -28,41 +28,49 @@ var display = function(){
 }
 
 
-var start = function(){
-	connection.query("SELECT * FROM products", function(err, res){
-			inquirer.prompt({
-			name: "ID",
-			type: "input",
-			message: "Enter the ID of the item you would like to purchase:"
-		}).then(function(answer1){
+
+var start = function() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        inquirer.prompt({
+            name: "ID",
+            type: "input",
+            message: "Enter the ID of the item you would like to purchase:"
+        }).then(function(answer1) {
 			if(answer1.ID > res.length){
 				console.log("Incorrect ID.");
+				connection.end();
 			}
-			else{
-				for (var i = 0; i < res.length; i++){
-					if (res[i].ItemID == parseInt(answer1.ID)){
-						var chosenItem = res[i];
-						inquirer.prompt({
-							name: "amount",
-							type: "input",
-							message: "How many would you like to purchase?"
-						}).then(function(answer2){
-							 if (chosenItem.StockQuantity > parseInt(answer2.amount)){
-							 	var amountLeft = chosenItem.StockQuantity - parseInt(answer2.amount);
-							 	// console.log(amountLeft);
-							 	// console.log(chosenItem.ItemID);
-	                            connection.query("UPDATE products SET StockQuantity = ? WHERE ItemID = ? ",[amountLeft,chosenItem.ItemID], function(err, res) {
-	                                console.log("Your purchase was made!");
-	                            });
-							 }else{
-							 	console.log("amount is too high");
-							 }
-						})
-					}
-				}
-			}
-		})
-	})
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].ItemID == parseInt(answer1.ID)) {
+                    var chosenItem = res[i];
+                    inquirer.prompt({
+                        name: "amount",
+                        type: "input",
+                        message: "How many would you like to purchase?"
+                    }).then(function(answer) {
+               
+                        if (chosenItem.StockQuantity > parseInt(answer.amount)) {
+                        	var amountLeft = chosenItem.StockQuantity - parseInt(answer.amount);
+                    
+                            connection.query("UPDATE products SET ? WHERE ?", [{
+                                StockQuantity : amountLeft
+                            }, {
+                                ItemID: chosenItem.ItemID
+                            }], function(err, res) {
+                            	console.log();
+                                console.log("Purchase made succesfully!");
+                                display();
+                                connection.end();    
+                            });
+                        } else {
+                            console.log("Wrong amount. Try again...");
+                            connection.end();
+                            
+                        }
+                    })
+                }
+            }
+        })
+    })
 }
-
 
